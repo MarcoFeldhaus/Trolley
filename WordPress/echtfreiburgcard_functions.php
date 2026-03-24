42247079ef88ac2ca4a22446aeb87ae1dc9d8887
Marco Feldhaus <m.feldhaus@trolleymaker.com>
2026-03-24 07:56:45 +0100

    23032026

--- WordPress/echtfreiburgcard_functions.php
+++ WordPress/echtfreiburgcard_functions.php
@@ -103,6 +103,9 @@ function dcwd_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
      if ( false !== strpos( $atts[ 'href' ], '[region_name]' ) ) {
           $atts[ 'href' ] = do_shortcode('[region_name]');
      }
+	if ( false !== strpos( $atts[ 'href' ], '[region_arbeitgeberportal_login_url]' ) ) {
+          $atts[ 'href' ] = do_shortcode('[region_arbeitgeberportal_login_url]');
+     }
 
      return $atts;
 }
@@ -264,6 +267,16 @@ function get_white_label_paypal_fee_per_voucher() {
      return $region_paypal_fee_per_voucher;
 }
 
+function get_white_label_partnerportal_login_url() {
+     $region_partnerportal_login_url = get_option('region_partnerportal_login_url', '');
+     return $region_partnerportal_login_url;
+}
+
+function get_white_label_arbeitgeberportal_login_url() {
+     $region_arbeitgeberportal_login_url = get_option('region_arbeitgeberportal_login_url', '');
+     return $region_arbeitgeberportal_login_url;
+}
+
 
 
 add_action('admin_init', 'add_white_label_settings_fields');
@@ -343,6 +356,13 @@ function add_white_label_settings_fields(){
      register_setting( 'general', 'region_interessenten_registrierung_url', 'esc_attr' );
      add_settings_field('region_interessenten_registrierung_url', '<label for="region_interessenten_registrierung_url">Interessenten Registrierung URL  ([region_interessenten_registrierung_url])</label>' , 'callback_input_interessenten_registrierung_url' , 'general' );
 
+		 register_setting( 'general', 'region_partnerportal_login_url', 'esc_attr' );
+     add_settings_field('region_partnerportal_login_url', '<label for="region_partnerportal_login_url">Partnerportal Login URL  ([region_partnerportal_login_url])</label>' , 'callback_input_partnerportal_login_url' , 'general' );
+	
+	 register_setting( 'general', 'region_arbeitgeberportal_login_url', 'esc_attr' );
+     add_settings_field('region_arbeitgeberportal_login_url', '<label for="region_arbeitgeberportal_login_url">Arbeitgeberportal Login URL  ([region_arbeitgeberportal_login_url])</label>' , 'callback_input_arbeitgeberportal_login_url' , 'general' );
+
+	
      register_setting( 'general', 'region_paypal_fee', 'esc_attr' );
      add_settings_field('region_paypal_fee', '<label for="region_paypal_fee">PayPal Gebühr pro Bestellung (in Prozent)</label>' , 'callback_input_paypal_fee' , 'general' );
 
@@ -461,6 +481,17 @@ function callback_input_kundenportal_login_url() {
      $value = get_white_label_kundenportal_login_url();
      echo '<input type="text" id="region_kundenportal_login_url" name="region_kundenportal_login_url" value="' . $value . '" size="110" />';
 }
+
+function callback_input_partnerportal_login_url() {
+     $value = get_white_label_partnerportal_login_url();
+     echo '<input type="text" id="region_partnerportal_login_url" name="region_partnerportal_login_url" value="' . $value . '" size="110" />';
+}
+
+function callback_input_arbeitgeberportal_login_url() {
+     $value = get_white_label_arbeitgeberportal_login_url();
+     echo '<input type="text" id="region_arbeitgeberportal_login_url" name="region_arbeitgeberportal_login_url" value="' . $value . '" size="110" />';
+}
+
 
 function callback_input_kundenportal_registrierung_url() {
      $value = get_white_label_kundenportal_registrierung_url();
@@ -641,6 +672,19 @@ function white_label_interessent_registrierung_url_shortcode() {
      return 'https://mycity.cards/interessent-registrierung?region=' . $region_name;
 }
 add_shortcode('region_interessent_registrierung_url', 'white_label_interessent_registrierung_url_shortcode'); 
+
+function white_label_partnerportal_login_url_shortcode() { 
+ 
+     return get_white_label_partnerportal_login_url();
+}
+add_shortcode('region_partnerportal_login_url', 'white_label_partnerportal_login_url_shortcode'); 
+
+function white_label_arbeitgeberportal_login_url_shortcode() { 
+ 
+     return get_white_label_arbeitgeberportal_login_url();
+}
+add_shortcode('region_arbeitgeberportal_login_url', 'white_label_arbeitgeberportal_login_url_shortcode'); 
+
 
 
 
